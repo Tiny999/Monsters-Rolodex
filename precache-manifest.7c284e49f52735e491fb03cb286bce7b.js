81bc4bc4e5c539d7bd77b4d0a358c0e3c56f1911
Abbas Salami <olawalesalami3@gmail.com>
2020-11-09 23:46:37 +0100

    Updates

---
+++ precache-manifest.7c284e49f52735e491fb03cb286bce7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe5c6dbe7b94cdcec0643b8d34d514c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d6395d79963e05e2e864",
+    "url": "/monsters-rolodex/static/css/main.8c85ab5a.chunk.css"
+  },
+  {
+    "revision": "acf3e36c3c2bb9ac9b15",
+    "url": "/monsters-rolodex/static/js/2.47ae780a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.47ae780a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6395d79963e05e2e864",
+    "url": "/monsters-rolodex/static/js/main.bc56805d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
